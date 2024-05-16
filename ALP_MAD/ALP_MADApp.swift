1ca0c7793c57ebd3e2b0c16a2487d7dcff2c1bb6
jhwrwe <126223887+jhwrwe@users.noreply.github.com>
2024-05-16 11:13:56 +0700

    Initial Commit

---
+++ ALP_MAD/ALP_MADApp.swift
@@ -0,0 +1,17 @@
+//
+//  ALP_MADApp.swift
+//  ALP_MAD
+//
+//  Created by MacBook Pro on 16/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ALP_MADApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
