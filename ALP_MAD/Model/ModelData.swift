94a36ca52f6b8f9c7218db21a708e18c9d1b50fa
jhwrwe <126223887+jhwrwe@users.noreply.github.com>
2024-05-23 20:36:52 +0700

    This is for the modeldata of our project, but its still unifinished

---
+++ ALP_MAD/Model/ModelData.swift
@@ -0,0 +1,8 @@
+//
+//  ModelData.swift
+//  ALP_MAD
+//
+//  Created by MacBook Pro on 23/05/24.
+//
+
+import Foundation
---
+++ ALP_MAD/Recipe.swift
@@ -0,0 +1,18 @@
+//
+//  Recipe.swift
+//  ALP_MAD
+//
+//  Created by MacBook Pro on 23/05/24.
+//
+
+import SwiftUI
+
+struct Recipe: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Recipe()
+}
